2f38b535d469544d49d3614ca8c8d9d4f8f95d3c
dCOMMICS <sanwurld@gmail.com>
2023-08-30 22:40:17 +0300

    Co-authored-by: Anh Tran <quanhea@users.noreply.github.com>

--- qw.js
+++ qw.js
@@ -2,3 +2,14 @@ let O = [..."/*+-"]
 let of = 0.reduce((a,v))=> (a[v])= new Function('a', 'b', `return a${v}b`),
 
 // this code works fine on my side//
+
+
+// leetcode//
+
+@return {function}
+
+const createHelloWorld = function() {
+    return function (...args) {
+        return "Hello World";
+    }
+};
